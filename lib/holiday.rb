b2a81ef2186960a019457f15847e2a6543bf18be
Marie Burns <mcburns2222@gmail.com>
2018-11-22 03:24:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday, supplies| 
-     binding.pry
+     #binding.pry
      if supplies == "BBQ"
       return holiday
     end
